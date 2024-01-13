572f3b42c5b9f75e20c3421c276fa57c95a7c591
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2024-01-13 11:16:28 +0530

    Using action creator for asynchronous network call and also getting initial Cart Items

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import { useEffect,useState } from 'react';
+import { sendCartData,getCartData, cartActions } from './store/Cart';
 
 function App() {
 
+  const dispatch=useDispatch()
   const showCart=useSelector(state=>state.cart.showCart)
   const items=useSelector(state=>state.cart.items)
-  const [loading,setLoading]=useState(false)
-  const [isError,setError]=useState(false)
-  const [success,setSuccess]=useState(false)
+  const loading =useSelector(state=>state.cart.loading)
+  const isError=useSelector(state=>state.cart.isError)
+  const success=useSelector(state=>state.cart.success)
+  const isInitialCart=useSelector(state=>state.cart.isInitialCart)
+  // const [loading,setLoading]=useState(false)
+  // const [isError,setError]=useState(false)
+  // const [success,setSuccess]=useState(false)
+  const [isInitial,setIsInitial]=useState(true)
+  // let isInitial=true
 
 
   useEffect(()=>{
-    const fetchCartItems=async()=>{
-      setLoading(true)
-      const response=await fetch(`https://react-http-92046-default-rtdb.firebaseio.com/cart.json`,{
-      method:'PUT',
-      body:JSON.stringify(items)
-    })
-    setLoading(false)
-    if(!response.ok){
-      setError(true)
-      setTimeout(()=>{
-        setError(false)
-      },2000)
-    }else{
-      setSuccess(true)
-      setTimeout(()=>{
-        setSuccess(false)
-      },2000)
+    // const fetchCartItems=async()=>{
+
+    //   // dispatch(sendCartData(items))
+
+    //   setLoading(true)
+    //   const response=await fetch(`https://react-http-92046-default-rtdb.firebaseio.com/cart.json`,{
+    //   method:'PUT',
+    //   body:JSON.stringify(items)
+    // })
+    // setLoading(false)
+    // if(!response.ok){
+    //   setError(true)
+    //   setTimeout(()=>{
+    //     setError(false)
+    //   },2000)
+    // }else{
+    //   setSuccess(true)
+    //   setTimeout(()=>{
+    //     setSuccess(false)
+    //   },2000)
+    // }
+    // }
+    // console.log(isInitial)
+    console.log(isInitialCart)
+    if(isInitialCart==true){
+      console.log(isInitialCart)
+      dispatch(cartActions.setInitialCart())
+      return
     }
+    if(isInitial==true){
+      // isInitial=false
+      setIsInitial(false)
+      dispatch(getCartData())
+      return
     }
-    if(items.length!==0){
-      console.log(items)
-      fetchCartItems()
-    }
+    // console.log(items)
+    
+    dispatch(sendCartData(items))
     // fetchCartItems()
   },[items])
 
--- src/store/Cart.js
+++ src/store/Cart.js
@@ -1,8 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialCartStore={
     items:[],
-    showCart:false
+    showCart:false,
+    loading:false,
+    isError:false,
+    success:false,
+    isInitialCart:false
 }
 
 const cartSlice=createSlice({
@@ -22,6 +27,16 @@ const cartSlice=createSlice({
             if(flag==false){
                 state.items.push(action.payload)
             }
+        },
+        setCartItems(state,action){
+            state.isInitialCart=true
+            // console.log(state.)
+            state.items=action.payload
+            console.log("make initial true")
+        },
+        setInitialCart(state){
+            state.isInitialCart=!state.isInitialCart
+            console.log("finally false")
         },
         increaseQuantity(state,action){
             state.items.forEach((item)=>{
@@ -53,9 +68,70 @@ const cartSlice=createSlice({
         },
         toggleCart(state){
             state.showCart=!state.showCart
+        },
+        setLoading(state){
+            state.loading=!state.loading
+        },
+        setError(state){
+            state.isError=!state.isError
+        },
+        setSuccess(state){
+            state.success=!state.success
         }
     }
 })
+
+
+export const sendCartData=(cartData)=>{
+    // console.log("came to sendcartdata")
+    return async(dispatch)=>{
+
+        const sendRequest=async()=>{
+            const response=await fetch(`https://expense-tracker-fdf40-default-rtdb.firebaseio.com/cart.json`,{
+      method:'PUT',
+      body:JSON.stringify(cartData)
+    })
+    // const response=await axios.post(`https://expense-tracker-fdf40-default-rtdb.firebaseio.com/cart.json`,expense)
+    if(!response.ok){
+        throw Error('error')
+    }
+        }
+        dispatch(cartActions.setLoading())
+        try {
+            await sendRequest()
+            console.log("try")
+            dispatch(cartActions.setLoading())
+            dispatch(cartActions.setSuccess())
+            setTimeout(()=>{
+                dispatch(cartActions.setSuccess())
+            },2000)
+        } catch (error) {
+            console.log("catch")
+            dispatch(cartActions.setLoading())
+            dispatch(cartActions.setError())
+            setTimeout(()=>{
+                dispatch(cartActions.setError())
+            },2000)
+        }
+    }
+}
+
+export const getCartData=()=>{
+
+    return async(dispatch)=>{
+    //     const response=await fetch(`https://expense-tracker-fdf40-default-rtdb.firebaseio.com/cart.json`,{
+    //     method:'GET'
+    //   })
+
+      const response=await axios.get(`https://expense-tracker-fdf40-default-rtdb.firebaseio.com/cart.json`)
+
+      console.log(response)
+
+      console.log("seeting initial cart items")
+      dispatch(cartActions.setCartItems(response.data))
+    }
+    
+}
 
 export const cartReducer=cartSlice.reducer
 export const cartActions=cartSlice.actions
